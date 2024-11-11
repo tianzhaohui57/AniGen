9d4b438d0a13d5b03df3b2b69bc42eefc95c146f
tianzhaohui57 <2735604537@qq.com>
2024-11-11 22:42:57 +0800

    2024/11/11

---
+++ git push.bat
@@ -0,0 +1,11 @@
+@echo off
+for /f "tokens=2 delims==" %%I in ('"wmic os get localdatetime /value"') do set datetime=%%I
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+set commit_date=%year%/%month%/%day%
+set http_proxy=http://192.168.101.114:9080
+set https_proxy=http://192.168.101.114:9080
+git add .
+git commit -m "%commit_date%"
+git push
